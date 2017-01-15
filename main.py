a4552c3adca22826894eb63d6064827ed4378024
Joaquim Monteiro <gmes.078@gmail.com>
2017-01-15 20:19:28 +0000

    Use single quotes instead of double quotes for consistency

--- main.py
+++ main.py
@@ -30,20 +30,20 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
 
         self.pushButton_close.clicked.connect(self.close)
 
-        self.comboBox_partscheme.insertItem(0, "MBR partition scheme for BIOS or UEFI")
-        self.comboBox_partscheme.insertItem(1, "MBR partition scheme for UEFI")
-        self.comboBox_partscheme.insertItem(2, "GPT partition scheme for UEFI")
+        self.comboBox_partscheme.insertItem(0, 'MBR partition scheme for BIOS or UEFI')
+        self.comboBox_partscheme.insertItem(1, 'MBR partition scheme for UEFI')
+        self.comboBox_partscheme.insertItem(2, 'GPT partition scheme for UEFI')
 
-        self.comboBox_filesystem.insertItem(0, "FAT")
-        self.comboBox_filesystem.insertItem(1, "FAT32")
-        self.comboBox_filesystem.insertItem(2, "NTFS")
-        self.comboBox_filesystem.insertItem(3, "UDF")
-        self.comboBox_filesystem.insertItem(4, "exFAT")
+        self.comboBox_filesystem.insertItem(0, 'FAT')
+        self.comboBox_filesystem.insertItem(1, 'FAT32')
+        self.comboBox_filesystem.insertItem(2, 'NTFS')
+        self.comboBox_filesystem.insertItem(3, 'UDF')
+        self.comboBox_filesystem.insertItem(4, 'exFAT')
 
-        self.comboBox_checkbadblocks.insertItem(0, "1 Pass")
-        self.comboBox_checkbadblocks.insertItem(1, "2 Passes")
-        self.comboBox_checkbadblocks.insertItem(2, "3 Passes")
-        self.comboBox_checkbadblocks.insertItem(3, "4 Passes")
+        self.comboBox_checkbadblocks.insertItem(0, '1 Pass')
+        self.comboBox_checkbadblocks.insertItem(1, '2 Passes')
+        self.comboBox_checkbadblocks.insertItem(2, '3 Passes')
+        self.comboBox_checkbadblocks.insertItem(3, '4 Passes')
 
         self.update_gui()
         self.comboBox_bootmethod.setCurrentIndex(0)
@@ -55,23 +55,23 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
             self.comboBox_device.addItem('(' + str(round(usb_info.get_size(device)/1073741824, 1)) + 'GiB) ' + device)
 
     def update_gui(self):
-        if self.comboBox_filesystem.currentText() == "exFAT" or self.comboBox_filesystem.currentText() == "UDF":
+        if self.comboBox_filesystem.currentText() == 'exFAT' or self.comboBox_filesystem.currentText() == 'UDF':
             self.checkBox_bootmethod.setCheckable(False)
         else:
             self.checkBox_bootmethod.setCheckable(True)
 
         bootmethod_current_index = self.comboBox_bootmethod.currentIndex()
         self.comboBox_bootmethod.clear()
-        self.comboBox_bootmethod.insertItem(0, "ISO Image")
-        self.comboBox_bootmethod.insertItem(1, "DD Image")
-        self.comboBox_bootmethod.insertItem(2, "UEFI:NTFS")
-        if self.comboBox_partscheme.currentText() == "MBR partition scheme for BIOS or UEFI":
-            self.comboBox_bootmethod.insertItem(3, "FreeDOS")
-            self.comboBox_bootmethod.insertItem(4, "Syslinux 4.07")
-            self.comboBox_bootmethod.insertItem(5, "Syslinux 6.03")
-            self.comboBox_bootmethod.insertItem(6, "ReactOS")
-            self.comboBox_bootmethod.insertItem(7, "Grub 2.02 ~beta3")
-            self.comboBox_bootmethod.insertItem(8, "Grub4DOS 0.4.6a")
+        self.comboBox_bootmethod.insertItem(0, 'ISO Image')
+        self.comboBox_bootmethod.insertItem(1, 'DD Image')
+        self.comboBox_bootmethod.insertItem(2, 'UEFI:NTFS')
+        if self.comboBox_partscheme.currentText() == 'MBR partition scheme for BIOS or UEFI':
+            self.comboBox_bootmethod.insertItem(3, 'FreeDOS')
+            self.comboBox_bootmethod.insertItem(4, 'Syslinux 4.07')
+            self.comboBox_bootmethod.insertItem(5, 'Syslinux 6.03')
+            self.comboBox_bootmethod.insertItem(6, 'ReactOS')
+            self.comboBox_bootmethod.insertItem(7, 'Grub 2.02 ~beta3')
+            self.comboBox_bootmethod.insertItem(8, 'Grub4DOS 0.4.6a')
         self.comboBox_bootmethod.setCurrentIndex(0)
         if bootmethod_current_index <= self.comboBox_bootmethod.count():
             self.comboBox_bootmethod.setCurrentIndex(bootmethod_current_index)
