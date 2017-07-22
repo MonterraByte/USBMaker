60c90f1cadf1e03bade3bb68e4bd0125918a12d7
Joaquim Monteiro <joaquim.monteiro@protonmail.com>
2017-07-22 20:42:34 +0100

    Add dependencies in setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup
 setup(name='USBMaker',
       version='1.0.0',
       packages=['USBMaker'],
+      install_requires=['PyQt5'],
       url='https://github.com/gmes/USBMaker',
       license='GPL',
       entry_points={'gui_scripts': ['usbmaker = USBMaker.main']})
