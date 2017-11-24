de36945c63444d3ddd5f878d34c899aa8d3bf606
Joaquim Monteiro <joaquim.monteiro@protonmail.com>
2017-11-24 22:07:26 +0000

    Start implementing the non graphical binary

---
+++ src/main.rs
@@ -0,0 +1,29 @@
+//   Copyright © 2017 Joaquim Monteiro
+//
+//   This file is part of USBMaker.
+//
+//   USBMaker is free software: you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by
+//   the Free Software Foundation, either version 3 of the License, or
+//   (at your option) any later version.
+//
+//   USBMaker is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU General Public License for more details.
+//
+//   You should have received a copy of the GNU General Public License
+//   along with USBMaker.  If not, see <https://www.gnu.org/licenses/>.
+
+#[macro_use]
+extern crate clap;
+
+use clap::App;
+
+fn main() {
+    App::new("USBMaker")
+        .author(crate_authors!())
+        .version(crate_version!())
+        .about("Create bootable usb drives")
+        .get_matches();
+}
