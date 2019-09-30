e6b4501117d8b14783c9e639a05b411ffab6126f
Joaquim Monteiro <joaquim.monteiro@protonmail.com>
2019-09-30 21:30:51 +0100

    Remove unnecessary options from the CLI

--- src/main.rs
+++ src/main.rs
@@ -34,187 +34,73 @@ use std::path::PathBuf;
 use std::process;
 
 use ansi_term::Colour;
-use clap::{App, Arg, SubCommand};
+use clap::{App, Arg};
 
 fn main() {
     let matches = App::new("USBMaker")
         .author(crate_authors!())
         .version(crate_version!())
         .about("Create bootable usb drives")
-        .subcommands(vec![
-            SubCommand::with_name("format")
-                .author(crate_authors!())
-                .version(crate_version!())
-                .about("Create a filesystem on a partition")
-                .args(&[
-                    Arg::with_name("partition")
-                        .takes_value(true)
-                        .required(true)
-                        .index(1)
-                        .help("Partition in which the filesystem will be created"),
-                    Arg::with_name("filesystem")
-                        .takes_value(true)
-                        .required(true)
-                        .index(2)
-                        .help("Type of filesystem to create")
-                        .possible_values(&["btrfs", "exfat", "ext2", "ext3", "ext4", "f2fs", "fat32", "ntfs", "udf", "xfs"]),
-                    Arg::with_name("badblocks")
-                        .short("b")
-                        .long("badblocks")
-                        .takes_value(false)
-                        .help("Check the device for bad blocks before formatting (only supported by ext{2,3,4}, fat32 and ntfs)"),
-                    Arg::with_name("device")
-                        .short("d")
-                        .long("device")
-                        .takes_value(true)
-                        .help("Treat target as device, creating a partition table and one partition before formatting")
-                        .possible_values(&["gpt", "msdos"]),
-                    Arg::with_name("label")
-                        .short("l")
-                        .long("label")
-                        .takes_value(true)
-                        .help("Set the label of the filesystem"),
-                    Arg::with_name("yes")
-                        .short("y")
-                        .long("yes")
-                        .takes_value(false)
-                        .help("Confirm prompts automatically"),
-                ]),
-            SubCommand::with_name("create-table")
-                .author(crate_authors!())
-                .version(crate_version!())
-                .about("Create a new partition table, erasing all data on the device")
-                .args(&[
-                    Arg::with_name("device")
-                        .takes_value(true)
-                        .required(true)
-                        .index(1)
-                        .help("Device to modify"),
-                    Arg::with_name("type")
-                        .takes_value(true)
-                        .required(true)
-                        .index(2)
-                        .help("Type of partition table to create")
-                        .possible_values(&["gpt", "msdos"]),
-                    Arg::with_name("partition")
-                        .short("p")
-                        .long("partition")
-                        .takes_value(false)
-                        .help("Create a partition occupying the entire device"),
-                    Arg::with_name("yes")
-                        .short("y")
-                        .long("yes")
-                        .takes_value(false)
-                        .help("Confirm prompts automatically"),
-                ]),
-            SubCommand::with_name("iso")
-                .author(crate_authors!())
-                .version(crate_version!())
-                .about("Create a bootable USB drive using the contents of an ISO file")
-                .args(&[
-                    Arg::with_name("device")
-                        .takes_value(true)
-                        .required(true)
-                        .index(1)
-                        .help("Device to make bootable"),
-                    Arg::with_name("iso")
-                        .takes_value(true)
-                        .required(true)
-                        .help("ISO file to use as source"),
-                    Arg::with_name("badblocks")
-                        .short("b")
-                        .long("badblocks")
-                        .takes_value(false)
-                        .help("Check the device for bad blocks before formatting (only supported by ext{2,3,4}, fat32 and ntfs)"),
-                    Arg::with_name("filesystem")
-                        .short("f")
-                        .long("filesystem")
-                        .takes_value(true)
-                        .help("Type of filesystem to create")
-                        .possible_values(&["btrfs", "exfat", "ext2", "ext3", "ext4", "f2fs", "fat32", "ntfs", "udf", "xfs"])
-                        .default_value("fat32"),
-                    Arg::with_name("label")
-                        .short("l")
-                        .long("label")
-                        .takes_value(true)
-                        .help("Set the label of the filesystem"),
-                    Arg::with_name("table")
-                        .short("t")
-                        .long("table")
-                        .takes_value(true)
-                        .help("Type of partition table to create")
-                        .possible_values(&["gpt", "msdos"])
-                        .default_value("msdos"),
-                    Arg::with_name("yes")
-                        .short("y")
-                        .long("yes")
-                        .takes_value(false)
-                        .help("Confirm prompts automatically")
-                ]),
+        .args(&[
+            Arg::with_name("device")
+                .takes_value(true)
+                .required(true)
+                .index(1)
+                .help("Device to make bootable"),
+            Arg::with_name("iso")
+                .takes_value(true)
+                .required(true)
+                .help("ISO file to use as source"),
+            Arg::with_name("badblocks")
+                .short("b")
+                .long("badblocks")
+                .takes_value(false)
+                .help("Check the device for bad blocks before formatting (only supported by ext{2,3,4}, fat32 and ntfs)"),
+            Arg::with_name("filesystem")
+                .short("f")
+                .long("filesystem")
+                .takes_value(true)
+                .help("Type of filesystem to create")
+                .possible_values(&["btrfs", "exfat", "ext2", "ext3", "ext4", "f2fs", "fat32", "ntfs", "udf", "xfs"])
+                .default_value("fat32"),
+            Arg::with_name("label")
+                .short("l")
+                .long("label")
+                .takes_value(true)
+                .help("Set the label of the filesystem"),
+            Arg::with_name("table")
+                .short("t")
+                .long("table")
+                .takes_value(true)
+                .help("Type of partition table to create")
+                .possible_values(&["gpt", "msdos"])
+                .default_value("msdos"),
+            Arg::with_name("yes")
+                .short("y")
+                .long("yes")
+                .takes_value(false)
+                .help("Confirm prompts automatically")
         ])
         .get_matches();
 
-    match matches.subcommand() {
-        ("format", Some(sub_matches)) => {
-            let partition: PathBuf = PathBuf::from(
-                sub_matches
-                    .value_of("partition")
-                    .expect("No partition specified"),
-            );
+    let device: PathBuf = PathBuf::from(matches.value_of("device").expect("No device specified"));
 
-            if let Err(err) = formatting::format(
-                &partition,
-                sub_matches
-                    .value_of("filesystem")
-                    .expect("No filesystem specified"),
-                sub_matches.is_present("badblocks"),
-                sub_matches.value_of("device"),
-                sub_matches.value_of("label"),
-                sub_matches.is_present("yes"),
-            ) {
-                exit_with_error(err);
-            }
-        }
-        ("create-table", Some(sub_matches)) => {
-            let device: PathBuf =
-                PathBuf::from(sub_matches.value_of("device").expect("No device specified"));
+    let iso: PathBuf = PathBuf::from(matches.value_of("iso").expect("No device specified"));
 
-            if let Err(err) = partitioning::create_table(
-                &device,
-                sub_matches
-                    .value_of("type")
-                    .expect("No partition table type specified"),
-                sub_matches.is_present("yes"),
-                sub_matches.is_present("partition"),
-                None,
-            ) {
-                exit_with_error(err);
-            }
-        }
-        ("iso", Some(sub_matches)) => {
-            let device: PathBuf =
-                PathBuf::from(sub_matches.value_of("device").expect("No device specified"));
-
-            let iso: PathBuf =
-                PathBuf::from(sub_matches.value_of("iso").expect("No device specified"));
-
-            if let Err(err) = iso::create_bootable(
-                &device,
-                &iso,
-                sub_matches
-                    .value_of("filesystem")
-                    .expect("No filesystem specified"),
-                sub_matches
-                    .value_of("table")
-                    .expect("No partition table type specified"),
-                sub_matches.value_of("label"),
-                sub_matches.is_present("badblocks"),
-                sub_matches.is_present("yes"),
-            ) {
-                exit_with_error(err);
-            }
-        }
-        _ => (),
+    if let Err(err) = iso::create_bootable(
+        &device,
+        &iso,
+        matches
+            .value_of("filesystem")
+            .expect("No filesystem specified"),
+        matches
+            .value_of("table")
+            .expect("No partition table type specified"),
+        matches.value_of("label"),
+        matches.is_present("badblocks"),
+        matches.is_present("yes"),
+    ) {
+        exit_with_error(err);
     }
 }
 
