af28c4ac02e4ddc051a7412d90ddad6751a0b2ed
Joaquim Monteiro <joaquim.monteiro@protonmail.com>
2019-09-30 22:26:33 +0100

    Migrate argument parsing to structopt

--- src/main.rs
+++ src/main.rs
@@ -16,8 +16,6 @@
 //   along with USBMaker.  If not, see <https://www.gnu.org/licenses/>.
 
 extern crate ansi_term;
-#[macro_use]
-extern crate clap;
 extern crate indicatif;
 extern crate libparted;
 extern crate tempfile;
@@ -34,77 +32,46 @@ use std::path::PathBuf;
 use std::process;
 
 use ansi_term::Colour;
-use clap::{App, Arg};
+use structopt::StructOpt;
 
-fn main() {
-    let matches = App::new("USBMaker")
-        .author(crate_authors!())
-        .version(crate_version!())
-        .about("Create bootable usb drives")
-        .args(&[
-            Arg::with_name("device")
-                .takes_value(true)
-                .required(true)
-                .index(1)
-                .help("Device to make bootable"),
-            Arg::with_name("iso")
-                .takes_value(true)
-                .required(true)
-                .help("ISO file to use as source"),
-            Arg::with_name("badblocks")
-                .short("b")
-                .long("badblocks")
-                .takes_value(false)
-                .help("Check the device for bad blocks before formatting (only supported by ext{2,3,4}, fat32 and ntfs)"),
-            Arg::with_name("filesystem")
-                .short("f")
-                .long("filesystem")
-                .takes_value(true)
-                .help("Type of filesystem to create")
-                .possible_values(&["btrfs", "exfat", "ext2", "ext3", "ext4", "f2fs", "fat32", "ntfs", "udf", "xfs"])
-                .default_value("fat32"),
-            Arg::with_name("label")
-                .short("l")
-                .long("label")
-                .takes_value(true)
-                .help("Set the label of the filesystem"),
-            Arg::with_name("table")
-                .short("t")
-                .long("table")
-                .takes_value(true)
-                .help("Type of partition table to create")
-                .possible_values(&["gpt", "msdos"])
-                .default_value("msdos"),
-            Arg::with_name("yes")
-                .short("y")
-                .long("yes")
-                .takes_value(false)
-                .help("Confirm prompts automatically")
-        ])
-        .get_matches();
+use error::USBMakerError;
 
-    let device: PathBuf = PathBuf::from(matches.value_of("device").expect("No device specified"));
-
-    let iso: PathBuf = PathBuf::from(matches.value_of("iso").expect("No device specified"));
-
-    if let Err(err) = iso::create_bootable(
-        &device,
-        &iso,
-        matches
-            .value_of("filesystem")
-            .expect("No filesystem specified"),
-        matches
-            .value_of("table")
-            .expect("No partition table type specified"),
-        matches.value_of("label"),
-        matches.is_present("badblocks"),
-        matches.is_present("yes"),
-    ) {
-        exit_with_error(err);
-    }
+#[derive(StructOpt, Debug)]
+#[structopt(about, author)]
+struct Args {
+    /// Path to the ISO file
+    iso: PathBuf,
+    /// Path to the device file
+    device: PathBuf,
+    /// Check for bad blocks while formatting
+    #[structopt(short, long)]
+    badblocks: bool,
+    /// Filesystem used to format the device
+    #[structopt(short, long, possible_values = &["btrfs", "exfat", "ext2", "ext3", "ext4", "f2fs", "fat32", "ntfs", "udf", "xfs"], default_value = "fat32")]
+    filesystem: String,
+    /// Partition table used on the device
+    #[structopt(short, long, possible_values = &["gpt", "msdos"], default_value = "msdos")]
+    table: String,
+    /// Label to give to the filesystem (to use the default, don't pass this option)
+    #[structopt(short, long)]
+    label: Option<String>,
+    /// Do not prompt for confirmation
+    #[structopt(short = "y", long = "yes")]
+    force_yes: bool,
 }
 
-fn exit_with_error<T: error::USBMakerError>(error: T) -> ! {
-    eprintln!("{} {}", Colour::Red.bold().paint("Error:"), error);
-    process::exit(error.error_code());
+#[paw::main]
+fn main(args: Args) {
+    if let Err(err) = iso::create_bootable(
+        &args.device,
+        &args.iso,
+        &args.filesystem,
+        &args.table,
+        args.label.as_ref().map(|s| s.as_str()),
+        args.badblocks,
+        args.force_yes,
+    ) {
+        eprintln!("{} {}", Colour::Red.bold().paint("Error:"), err);
+        process::exit(err.error_code());
+    }
 }
